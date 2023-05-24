8fd71b356e875ef1816359c25a0d11762ec47efb
overlordpro-sys <brent.littlebean@gmail.com>
2023-05-24 12:57:15 -0700

    Refined and added script to open and edit the database

--- attendance.py
+++ attendance.py
@@ -1,24 +1,22 @@
 import PySimpleGUI as sg
 import mysql.connector
 from mysql.connector import Error
-import time
 from datetime import datetime
 from mfrc522 import SimpleMFRC522
 
 
-def confirm_window(first, last, team, id, mydb, cursor):
-    confirm_id = [sg.Text("ID: "), sg.Input(id, enable_events=False, disabled=True, key='ci_id')]
-    confirm_info = [sg.Text('Team: '), sg.Input(team, enable_events=False, disabled=True, key='ci_team'),
-                    sg.Text("Name: "),
-                    sg.Input(first + last, enable_events=False, disabled=True, key='ci_name')]
-    window = sg.Window("Attendance", [[sg.Text("Is this you?", justification='center')], confirm_id, confirm_info,
+def confirm_window(user_id, first, last, team, mydb, cursor):
+    window = sg.Window("Attendance", [[sg.Text("Is this you?", justification='center')],
+                                      [sg.Text("ID: "), sg.Input(user_id, enable_events=False, disabled=True)],
+                                      [sg.Text('Team: '), sg.Input(team, enable_events=False, disabled=True)],
+                                      [sg.Text("Name: "), sg.Input(first + " " + last, enable_events=False, disabled=True)],
                                       [sg.Button("Confirm", bind_return_key=True)]])
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     while True:
         event, values = window.read()
         if event == "Confirm":
-            cursor.execute("INSERT INTO attendance (check_in_time, check_in_id) VALUES (%s, %s)", (now, id))
+            cursor.execute("INSERT INTO attendance (check_in_time, check_in_id) VALUES (%s, %s)", (now, user_id))
             mydb.commit()
             print(cursor.rowcount)
             break
@@ -41,15 +39,14 @@ def main():
         print("Error with MySQL", e)
         exit()
 
-    ci_title = sg.Text("Tap your card/tag to the scanner to check in", key='ci_title')
+    ci_title = sg.Text("Tap your card/tag to the scanner to check in")
     ci_db_button = sg.Button("See DB")
-
     ci_screen = [[ci_title, sg.Push(), ci_db_button]]
 
     db_screen = [[sg.Text("DB_placeholder"), sg.Button("Back")]]
 
     # Create the layout for switching screens
-    layout = [[sg.Column(ci_screen, key='ci_screen'), sg.Column(db_screen, visible=False, key='db_screen')],
+    layout = [[sg.Column(ci_screen, key='ci_screen'), sg.Column(db_screen, visible=False)],
               ]
     # Create the window
     window = sg.Window("Attendance", layout)
@@ -69,18 +66,20 @@ def main():
             window['db_screen'].update(visible=False)
         # if on the check in screen, poll for successful scan
         if window['ci_screen'].visible:
-            id, text = reader.read_no_block()
-            if id is not None:
+            user_id, text = reader.read_no_block()
+            if user_id is not None:
                 # if True:  # placeholder for id scanner
-                cursor.execute("SELECT first_name, last_name, team_section FROM members WHERE member_id = (%s)", (id,))
+                cursor.execute("SELECT first_name, last_name, team_section FROM members WHERE member_id = (%s)", (user_id,))
                 row = cursor.fetchone()
                 if row:
-                    confirm_window(row[0], row[1], row[2], id, mydb, cursor)
+                    confirm_window(user_id, row[0], row[1], row[2], mydb, cursor)
                 else:
-                    sg.popup_error("ID not found in database", title="Error", bind_return_key=True)
+                    sg.popup_error("ID not found in database", title="Error", grab_anywhere=True)
         if event == sg.WIN_CLOSED:
             break
+    # cleanup
     window.close()
+    GPIO.cleanup()
     if mydb.is_connected():
         cursor.close()
         mydb.close()
---
+++ register.py
@@ -0,0 +1,196 @@
+import PySimpleGUI as sg
+import mysql.connector
+from mysql.connector import Error
+from datetime import datetime
+from mfrc522 import SimpleMFRC522
+
+PAGE_SIZE = 10  # Change this to whatever page size you want
+
+
+# window to edit the user data associated with an rfid tag in the database
+def confirm_window(user_id, mydb, cursor, first="", last="", team=""):
+    window = sg.Window("Attendance", [[sg.Text("Is this correct?", justification='center')],
+                                      [sg.Text("ID: "),
+                                       sg.Input(user_id, key='input_id', disabled=True, enable_events=False)],
+                                      [sg.Listbox(
+                                          values=["Programming", "Mechanical", "Shop", "Marketing", "Electrical"],
+                                          key='select_team', select_mode='single', default_values=[team])],
+                                      [sg.Text("Team: "), sg.Input(team, key='input_team')],
+                                      [sg.Text("First: "), sg.Input(first, key='input_first')],
+                                      [sg.Text("Last: "), sg.Input(last, key='input_last')],
+                                      [sg.Button("Cancel"), sg.Button("Save Changes")]])
+    while True:
+        event, values = window.read()
+        user_id = values['input_id']
+        # team = values['input_team']
+        team = values['select_team'][0]
+        first = values['input_first']
+        last = values['input_last']
+        if event == "Save Changes":
+            # if duplicate id (card has already been used before) updates instead of adding new entry
+            cursor.execute(
+                "INSERT INTO members (member_id, first_name, last_name, team_section) VALUES (%s, %s, %s, %s) "
+                "ON DUPLICATE KEY UPDATE first_name = VALUES(first_name), last_name = VALUES("
+                "last_name), team_section = VALUES(team_section)",
+                (user_id, first, last, team))
+            mydb.commit()
+            break
+        if event == sg.WIN_CLOSED or event == "Cancel":
+            break
+    window.close()
+
+
+def fetch_page(table, page):
+    start_index = page * PAGE_SIZE
+    mycursor.execute(f"SELECT * FROM {table} LIMIT {start_index}, {PAGE_SIZE}")
+    return mycursor.fetchall()
+
+
+# window to view and edit the database
+def db_window(mydb, cursor):
+    page_num = 0
+    selected_table = 'members'
+    # Define the window layout
+    layout = [
+        [sg.Button('Show members table'), sg.Button('Show attendance table')],
+        [sg.Multiline(size=(60, 20), key='-OUTPUT-', enable_events=True)],
+        [sg.Button('Previous Page'), sg.Button('Next Page')]
+    ]
+
+    while True:
+        event, values = window.read()
+        # End program if user closes window or presses the 'Quit' button
+        if event == sg.WINDOW_CLOSED:
+            break
+
+        elif event == 'Show members table':
+            selected_table = 'members'
+            result = fetch_page(selected_table, page_num)
+            window['-OUTPUT-'].update('\n'.join(str(row) for row in result))
+
+        elif event == 'Show attendance table':
+            selected_table = 'attendance'
+            result = fetch_page(selected_table, page_num)
+            window['-OUTPUT-'].update('\n'.join(str(row) for row in result))
+
+        elif event == '-OUTPUT-':  # Triggered when a line in the Multiline element is clicked
+            selected_row = values['-OUTPUT-'].split('\n')[0]  # This assumes each row is on a separate line
+            # Double click detection
+            if event == window.last_button_clicked:
+                if selected_table == 'members':
+                    edit_member_entry(mydb, cursor, selected_row)
+
+                # Refresh table
+                result = fetch_page(selected_table, page_num)
+                window['-OUTPUT-'].update('\n'.join(str(row) for row in result))
+
+        elif event == 'Previous Page':
+            if page_num > 0:
+                page_num -= 1
+                result = fetch_page(selected_table, page_num)
+                window['-OUTPUT-'].update('\n'.join(str(row) for row in result))
+
+        elif event == 'Next Page':
+            page_num += 1
+            result = fetch_page(selected_table, page_num)
+            window['-OUTPUT-'].update('\n'.join(str(row) for row in result))
+
+
+# opens window to edit a row in the members table
+def edit_member_entry(mydb, cursor, selected_row):
+    member_id, first, last, team = selected_row.split(',')  # Assumes first column is a unique identifier
+    window = sg.Window("Edit member", [[sg.Text("ID: "), sg.Input(member_id, key='input_id', disabled=True, enable_events=False)],
+                                       [sg.Listbox(
+                                           values=["Programming", "Mechanical", "Shop", "Marketing", "Electrical"],
+                                           key='select_team', select_mode='single', default_values=[team])],
+                                       [sg.Text("Team: "), sg.Input(team, key='input_team')],
+                                       [sg.Text("First: "), sg.Input(first, key='input_first')],
+                                       [sg.Text("Last: "), sg.Input(last, key='input_last')],
+                                       [sg.Button("Cancel"), sg.Button("Save Changes")]])
+    while True:
+        event, values = window.read()
+        member_id = values['input_id']
+        # team = values['input_team']
+        team = values['select_team'][0]
+        first = values['input_first']
+        last = values['input_last']
+        if event == "Save Changes":
+            # if duplicate id (card has already been used before) updates instead of adding new entry
+            cursor.execute("UPDATE members SET first_name = %s, last_name = %s, team_section = %s WHERE member_id = %s", (first, last, team, member_id))
+            mydb.commit()
+            break
+        if event == sg.WIN_CLOSED or event == "Cancel":
+            break
+    window.close()
+
+
+# opens window to edit a row in the attendance table
+def edit_attendance_entry(mydb, cursor, selected_row):
+    timestamp, member_id = selected_row.split(',')
+    placeholder = timestamp
+    window = sg.Window("Edit Attendance",
+                       [[sg.Text("Timestamp: "), sg.Input(member_id, key='input_time')],
+                        [sg.Text("ID: "), sg.Input(team, key='input_id')],
+                        [sg.Button("Cancel"), sg.Button("Save Changes")]])
+    while True:
+        event, values = window.read()
+        timestamp = values["input_time"]
+        member_id = values['input_id']
+        if event == "Save Changes":
+            # if duplicate id (card has already been used before) updates instead of adding new entry
+            cursor.execute("UPDATE attendance SET check_in_time = %s, check_in_id = %s WHERE check_in_time = %s", (timestamp, member_id, placeholder))
+            mydb.commit()
+            break
+        if event == sg.WIN_CLOSED or event == "Cancel":
+            break
+    window.close()
+
+
+def main():
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            database='attendance',
+            user="pi4470",
+            password="pi4470"
+        )
+        if mydb.is_connected():
+            cursor = mydb.cursor()
+    except Error as e:
+        print("Error with MySQL", e)
+        exit()
+
+    window = sg.Window("Attendance", [[sg.Text("Tap a card/tag to the scanner to view/edit current info")],
+                                      [sg.Button("Edit Database")]])
+
+    # Initialize the scanner
+    reader = SimpleMFRC522()
+    # Define page size
+
+    # Create an event loop
+    while True:
+        event, values = window.read(timeout=250)  # moves on to check reader after 250ms
+        # if on the check in screen, poll for successful scan
+        user_id, text = reader.read_no_block()
+        if event == "Edit Database":
+            db_window(mydb, cursor)
+        if user_id is not None:
+            # if True:  # placeholder for id scanner
+            cursor.execute("SELECT first_name, last_name, team_section FROM members WHERE member_id = (%s)", (user_id,))
+            row = cursor.fetchone()
+            if row:
+                confirm_window(user_id, mydb, cursor, row[0], row[1], row[2])
+            else:
+                confirm_window(user_id, mydb, cursor)
+        if event == sg.WIN_CLOSED:
+            break
+    # clean up
+    window.close()
+    GPIO.cleanup()
+    if mydb.is_connected():
+        cursor.close()
+        mydb.close()
+
+
+if __name__ == '__main__':
+    main()
